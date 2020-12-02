24c9304190e6109e421a54a0ba129f6193f3cef4
Felipe Baumgartel <40307228+FeBaumgartel@users.noreply.github.com>
2020-12-01 22:12:15 -0300

    feat: noticias

--- server/routes.js
+++ server/routes.js
@@ -1,21 +1,15 @@
 const express = require('express');
 
 const cotacoesController = require('./controllers/cotacoesController');
-<<<<<<< HEAD
 const tabelaController = require('./controllers/topEmpresasController');
-=======
 const noticiasController = require('./controllers/noticiasController');
->>>>>>> 821cdf7c65004bc836aadacf13c82f8ab446e75e
 
 const routes = express.Router();
 
 //rota para as cotações
 routes.get('/cotacoes', cotacoesController.getCotacoes);
-<<<<<<< HEAD
 routes.get('/tabela', tabelaController.getTabela);
-=======
 routes.get('/noticias', noticiasController.getNoticias);
->>>>>>> 821cdf7c65004bc836aadacf13c82f8ab446e75e
 
 
 module.exports = routes;
